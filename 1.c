d8e7679ad0f2e9df8df9ac5d9d57378944c484f1
pedram31 <pedram@eyelevate.com>
2017-03-16 08:15:27 +0900

    'update'

---
+++ 1.c
@@ -0,0 +1,66 @@
+//1
+
+#include <stdio.h>
+int main()
+{
+    int firstNumber, secondNumber, sumOfTwoNumbers;
+    
+    printf("Enter two integers: ");
+
+    // Two integers entered by user is stored using scanf() function
+    scanf("%d %d", &firstNumber, &secondNumber);
+
+    // sum of two numbers in stored in variable sumOfTwoNumbers
+    sumOfTwoNumbers = firstNumber + secondNumber;
+
+    // Displays sum      
+    printf("%d + %d = %d", firstNumber, secondNumber, sumOfTwoNumbers);
+
+    return 0;
+}
+
+
+//2
+#include <stdio.h>
+int main()
+{
+    int integerType;
+    float floatType;
+    double doubleType;
+    char charType;
+
+    // Sizeof operator is used to evaluate the size of a variable
+    printf("Size of int: %ld bytes\n",sizeof(integerType));
+    printf("Size of float: %ld bytes\n",sizeof(floatType));
+    printf("Size of double: %ld bytes\n",sizeof(doubleType));
+    printf("Size of char: %ld byte\n",sizeof(charType));
+
+    return 0;
+}
+
+
+//3
+
+#include <stdio.h>
+int main()
+{
+    double n1, n2, n3;
+
+    printf("Enter three numbers: ");
+    scanf("%lf %lf %lf", &n1, &n2, &n3);
+
+    if( n1>=n2 && n1>=n3 )
+        printf("%.2f is the largest number.", n1);
+
+    if( n2>=n1 && n2>=n3 )
+        printf("%.2f is the largest number.", n2);
+
+    if( n3>=n1 && n3>=n2 )
+        printf("%.2f is the largest number.", n3);
+
+    return 0;
+}
+
+
+
+
